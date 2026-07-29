7665e5eff43686e3c37f93aeb40c95a57c5855c4
sofamilypv-cmd <so.family.pv@gmail.com>
2026-07-29 15:10:31 -0700

    boost update

---
+++ Demo/Assets/Scripts/Player/LoseAdvantage.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoseAdvantage : MonoBehaviour
+{
+    public Meter playerOneMeter;
+    public Meter playerTwoMeter;
+    public Meter meter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    /*
+        if(playerOneMeter <= -10)
+        {
+            meter.BoostOne();
+        }
+        if(playerTwoMeter <= -10)
+        {
+            meter.BoostTwo();
+        }
+    */
+    }
+}
