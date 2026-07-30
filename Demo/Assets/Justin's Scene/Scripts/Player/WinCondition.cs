be3dd2b78b247f4a93f16bdc5969ae1adcf1b625
sofamilypv-cmd <so.family.pv@gmail.com>
2026-07-30 14:53:09 -0700

    hi

--- Demo/Assets/Justin's Scene/Scripts/Player/WinCondition.cs
+++ Demo/Assets/Justin's Scene/Scripts/Player/WinCondition.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class WinCondition : MonoBehaviour
 {
+    public Meter playerOneMeter;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,9 @@ public class WinCondition : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(playerOneMeter.playerOneMeter >= 24)
+        {
+            SceneManager.LoadScene("Player 1 Winning Screen");
+        }
     }
 }
